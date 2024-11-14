5d04da32995cba9d3ee3acb0028c1a7a2824f970
Diogo Pina <diogojpina@gmail.com>
2024-11-14 18:03:27 -0300

    Add entities to React

---
+++ packages/client/src/entities/hotel.entity.ts
@@ -0,0 +1,13 @@
+export type Hotel = {
+  _id: string;
+  chain_name: string;
+  hotel_name: string;
+  addressline1: string;
+  addressline2: string;
+  zipcode: number;
+  city: string;
+  state: string;
+  country: string;
+  countryisocode: string;
+  star_rating: number;
+};
